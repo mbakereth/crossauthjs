f72b5e4b9ad206267c9a1912afac30631b513c50
mabaker <matthew.baker@id.ethz.ch>
2024-11-21 09:27:17 +0100

    For LDAP testing, get settings from environment.  Now works for ETH LDAP.

--- packages/backend/src/authenticators/tests/excl_ldapauth.test.ts
+++ packages/backend/src/authenticators/tests/excl_ldapauth.test.ts
@@ -5,50 +5,66 @@ import { LdapUserStorage } from '../../storage/ldapstorage';
 import { LdapAuthenticator } from '../ldapauth';
 import { UserInputFields } from '@crossauth/common';
 
+function getAuth() {
+    const ldapUrls = process.env["LDAPURLS"] ? process.env["LDAPURLS"].split(",") : ["ldap://localhost:1389"]
+    const ldapUserSearchBase = process.env["LDAPSEARCH"] ?? "ou=users,dc=example,dc=org";
+    const ldapUsernameAttribute = process.env["LDAPUSERNAMEATTR"] ?? "cn";
+    const username = process.env["LDAPUSER"] ?? "dave";
+    const password = process.env["LDAPPASSWORD"] ?? "davePass123";
+    const email = process.env["LDAPEMAIL"] ?? "dave@dave.com";
+
+    return {ldapUrls, ldapUserSearchBase, ldapUsernameAttribute, username, password, email};
+}
 test('Ldapauth.authenticateUserInLdapAndLocal', async () => {
+    const {ldapUrls, ldapUserSearchBase, ldapUsernameAttribute, username, password, email} = getAuth();
+
     const localStorage = new InMemoryUserStorage();
     const ldapStorage = new LdapUserStorage(localStorage, {
-        ldapUrls: ["ldap://localhost:1389"],
-        ldapUserSearchBase: "ou=users,dc=example,dc=org",
-        ldapUsernameAttribute: "cn",
+        ldapUrls,
+        ldapUserSearchBase,
+        ldapUsernameAttribute,
     });
     const auth = new LdapAuthenticator(ldapStorage, {
         ldapAutoCreateAccount: false,
     })
     await ldapStorage.createUser(
-        {username: "dave", state: "active", email: "dave@dave.com"}, 
-        {password: "davePass123"});
-    const {user} = await ldapStorage.getUserByUsername("dave");
-    await auth.authenticateUser(user, {}, {password: "davePass123"});
+        {username, state: "active", email}, 
+        {password});
+    const {user} = await ldapStorage.getUserByUsername(username);
+    await auth.authenticateUser(user, {}, {password});
 });
 
 test('Ldapauth.authenticateUserInLdapNotInLocal', async () => {
+    const {ldapUrls, ldapUserSearchBase, ldapUsernameAttribute, username} = getAuth();
+
     const localStorage = new InMemoryUserStorage();
     const ldapStorage = new LdapUserStorage(localStorage, {
-        ldapUrls: ["ldap://localhost:1389"],
-        ldapUserSearchBase: "ou=users,dc=example,dc=org",
-        ldapUsernameAttribute: "cn",
+        ldapUrls,
+        ldapUserSearchBase,
+        ldapUsernameAttribute,
     });
-    await expect(async () => {await ldapStorage.getUserByUsername("dave")}).rejects.toThrowError();
+    await expect(async () => {await ldapStorage.getUserByUsername(username)}).rejects.toThrowError();
 });
 
 test('Ldapauth.authenticateUserAutoCreate', async () => {
+    const {ldapUrls, ldapUserSearchBase, ldapUsernameAttribute, username, password, email} = getAuth();
+
     const localStorage = new InMemoryUserStorage();
     const ldapStorage = new LdapUserStorage(localStorage, {
-        ldapUrls: ["ldap://localhost:1389"],
-        ldapUserSearchBase: "ou=users,dc=example,dc=org",
-        ldapUsernameAttribute: "cn",
+        ldapUrls,
+        ldapUserSearchBase,
+        ldapUsernameAttribute,
     });
     const auth = new LdapAuthenticator(ldapStorage, {
         ldapAutoCreateAccount: true,
     })
     const user : UserInputFields = {
-        username: "dave",
+        username,
         state: "active",
-        email: "dave@dave.com",
+        email,
     }
-    await auth.authenticateUser(user, {}, {password: "davePass123"});
-    const {user: newUser} = await ldapStorage.getUserByUsername("dave")
-    expect(newUser.email).toBe("dave@dave.com");
+    await auth.authenticateUser(user, {}, {password});
+    const {user: newUser} = await ldapStorage.getUserByUsername(username)
+    expect(newUser.email).toBe(email);
 
 });
--- packages/backend/src/storage/tests/excl_ldapstorage.test.ts
+++ packages/backend/src/storage/tests/excl_ldapstorage.test.ts
@@ -4,15 +4,32 @@ import { InMemoryUserStorage } from '../inmemorystorage';
 import { LdapUserStorage } from '../ldapstorage';
 
 test('LdapStorage.createUser', async () => {
-    const localStorage = new InMemoryUserStorage();
-    const ldapStorage = new LdapUserStorage(localStorage, {
-        ldapUrls: ["ldap://localhost:1389"],
-        ldapUserSearchBase: "ou=users,dc=example,dc=org",
-        ldapUsernameAttribute: "cn",
-    });
-    await ldapStorage.createUser(
-        {username: "dave", state: "active", email: "dave@dave.com"}, 
-        {password: "davePass123"});
-    const {user} = await localStorage.getUserByUsername("dave");
-    expect(user.email).toBe("dave@dave.com");
-});
+    if (!process.env["LDAPUSER"] || !process.env["LDAPPASSWORD"] || !process.env["LDAPEMAIL"] || !process.env["LDAPURLS"]) {
+        console.log("Skipping external LDAP test")
+        return;
+    }
+
+    const ldapUrls = process.env["LDAPURLS"] ? process.env["LDAPURLS"].split(",") : ["ldap://localhost:1389"]
+    const ldapUserSearchBase = process.env["LDAPSEARCH"] ?? "ou=users,dc=example,dc=org";
+    const ldapUsernameAttribute = process.env["LDAPUSERNAMEATTR"] ?? "cn";
+    const username = process.env["LDAPUSER"] ?? "dave";
+    const password = process.env["LDAPPASSWORD"] ?? "davePass123";
+    const email = process.env["LDAPEMAIL"] ?? "dave@dave.com";
+    try {
+        const localStorage = new InMemoryUserStorage();
+        const ldapStorage = new LdapUserStorage(localStorage, {
+            ldapUrls,
+            ldapUserSearchBase,
+            ldapUsernameAttribute,
+        });
+        await ldapStorage.createUser(
+            {username, state: "active", email}, 
+            {password});
+        const {user} = await localStorage.getUserByUsername(process.env["LDAPUSER"]);
+        expect(user.email).toBe(process.env["LDAPEMAIL"]);    
+    } catch (e) {
+        console.log(e);
+        expect(e).toBeUndefined();
+    }
+
+})
