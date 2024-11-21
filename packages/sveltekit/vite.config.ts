b9a76c44450d695b59623662ab7744cd85a531aa
mabaker <matthew.baker@id.ethz.ch>
2024-11-21 13:27:15 +0100

    Exgternalize sveltekit

--- packages/sveltekit/vite.config.ts
+++ packages/sveltekit/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
 		rollupOptions: {
 		  // make sure to externalize deps that shouldn't be bundled
 		  // into your library
-		  external: ['.env', '.env.unittest', 'prisma', '@prisma/client', 'node:crypto', 'crypto', ],
+		  external: ['.env', '.env.unittest', 'prisma', '@prisma/client', 'node:crypto', 'crypto', '@sveltejs/kit'],
 		},
 	  },
 	plugins: [
