7d37bc3a4ff1debe8f098e6770d432c2aba31236
Matthew Baker <matt@mac2.home>
2025-07-06 12:27:31 +0200

    Add pnpminstall.sh

---
+++ pnpminstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+packages="packages/common packages/backend packages/fastify packages/sveltekit packages/frontend"
+for dir in $packages; do
+    echo "Building $dir..."
+    (cd $dir; pnpm install)
+done
+
