ec7135e695295d3146b66adce1569d0fa58bff20
Matthew Baker <matt@mattbaker.ch>
2026-01-07 13:59:50 +0100

    Force option on git tag

--- tag.sh
+++ tag.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+force=""
+if [ $# -gt 0 ]; then
+    force="-f"
+fi
+exit 0
 tag=`cat VERSION`
 if [ $# = 1 ]; then
     msg=$1
 else
     msg="Tag version $tag"
 fi
-git tag -a "v$tag" -m "$msg"
+git tag $force -a "v$tag" -m "$msg"
 git push origin tag "v$tag"
 
 
